f2c3b428cb8ef0a51a609b56a89de8142b97b09c
Jenuel Ganawed <jenuelganawed936@gmail.com>
2024-06-16 21:00:20 +0800

    Updates

--- web-next/astro.config.mjs
+++ web-next/astro.config.mjs
@@ -1,11 +1,20 @@
 import { defineConfig } from "astro/config";
 import UnoCSS from "unocss/astro";
+// import node from "@astrojs/node";
+
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [
-        UnoCSS({
-            injectReset: true,
-        }),
-    ],
+  output: "server",
+  integrations: [UnoCSS({
+    injectReset: true
+  })]
+  // adapter: node({
+  //     mode: "standalone",
+  // }),
+  ,
+  adapter: node({
+    mode: "standalone"
+  })
 });
