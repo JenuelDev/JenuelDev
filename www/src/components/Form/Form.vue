5d39ffdc5e4f4ef760c6de1fe08f3e0c9b262778
Jenuel Oras Ganawed <me@jenuel.dev>
2025-05-06 14:34:37 +0800

    updates: make form sender work again

--- www/src/components/Form/Form.vue
+++ www/src/components/Form/Form.vue
@@ -1,22 +1,19 @@
 <script setup lang="ts">
-import { reactive, ref } from 'vue';
-import Alert from './../alert/Alert.vue';
-// import { reactive, ref } from '@vue/reactivity';
-// import emailjs, { init } from 'emailjs-com';
-// init('user_zdO7SqNAzUeW1bl8KtMhn');
-
+import { reactive, ref } from "vue";
+import Alert from "./../alert/Alert.vue";
+import emailjs from "@emailjs/browser";
 
 const form = reactive({
-    subject: '',
-    name: '',
-    email: '',
-    message: '',
+    subject: "",
+    name: "",
+    email: "",
+    message: "",
 });
 
 const alertShow = ref(false);
-const alertTitle = ref('');
-const alertDescription = ref('');
-const alertType = ref('success');
+const alertTitle = ref("");
+const alertDescription = ref("");
+const alertType = ref("success");
 const showError = ref(false);
 const sent = ref(false);
 const sending = ref(false);
@@ -27,7 +24,7 @@ const validateEmail = (email: string) => {
     return re.test(String(email).toLowerCase());
 };
 const checkProperties = () => {
-    return form.subject == '' || form.name == '' || form.email == '' || form.message == '' ? true : false;
+    return form.subject == "" || form.name == "" || form.email == "" || form.message == "" ? true : false;
 };
 
 const alertSet = ({ title, description, type, show }: any) => {
@@ -42,21 +39,21 @@ const validateForm = async (sentSuccess = false) => {
         alertSet({
             title: `Successfully Sent! I'll Check them later. 😁👍`,
             description: `It seams that your message was send successfuly. I'm not online always but I will definitely check your message when I go online.`,
-            type: 'success',
+            type: "success",
             show: true,
         });
-        form.subject = '';
-        form.name = '';
-        form.email = '';
-        form.message = '';
+        form.subject = "";
+        form.name = "";
+        form.email = "";
+        form.message = "";
         return;
     }
 
     if (checkProperties() === true) {
         alertSet({
-            title: 'Some Fields Are Empty 😢😭📧',
-            description: 'Sorry about that, it seems that there are empty fields. Please fill them all out.',
-            type: 'error',
+            title: "Some Fields Are Empty 😢😭📧",
+            description: "Sorry about that, it seems that there are empty fields. Please fill them all out.",
+            type: "error",
             show: true,
         });
 
@@ -64,9 +61,9 @@ const validateForm = async (sentSuccess = false) => {
     }
     if (validateEmail(form.email) === false) {
         alertSet({
-            title: 'Oops! Invalid Email 😢😭📧',
-            description: 'Sorry about that, it seems that you have entered a wrong email format.',
-            type: 'error',
+            title: "Oops! Invalid Email 😢😭📧",
+            description: "Sorry about that, it seems that you have entered a wrong email format.",
+            type: "error",
             show: true,
         });
         return false;
@@ -76,56 +73,95 @@ const validateForm = async (sentSuccess = false) => {
 };
 
 async function sendEmail() {
-    // alertShow.value = false;
-    // sending.value = true;
-    // if (await validateForm()) {
-    //     emailjs
-    //         .send('service_88wvqn9', 'template_nulphu2', form)
-    //         .then(() => {
-    //             sent.value = true;
-    //             showError.value = false;
-    //             sending.value = false;
-    //             validateForm(true);
-    //         })
-    //         .catch((e: any) => {
-    //             sent.value = false;
-    //             showError.value = false;
-    //             sending.value = false;
+    alertShow.value = false;
+    sending.value = true;
+    if (await validateForm()) {
+        emailjs
+            .send("service_88wvqn9", "template_nulphu2", form, {
+                publicKey: "user_zdO7SqNAzUeW1bl8KtMhn",
+            })
+            .then(
+                () => {
+                    sent.value = true;
+                    showError.value = false;
+                    sending.value = false;
+                    validateForm(true);
+                },
+                (error) => {
+                    sent.value = false;
+                    showError.value = false;
+                    sending.value = false;
 
-    //             if (e.response.status == 500) {
-    //                 alertSet({
-    //                     title: 'Oops! Email Does not Exist! 😢😭📧',
-    //                     description: 'Sorry about that, it seems that you have entered a wrong email, I cant find it anywhere.',
-    //                     type: 'error',
-    //                     show: true,
-    //                 });
-    //             }
-    //         });
-    // } else {
-    //     sending.value = false;
-    // }
+                    if (error.response.status == 500) {
+                        alertSet({
+                            title: "Oops! Email Does not Exist! 😢😭📧",
+                            description:
+                                "Sorry about that, it seems that you have entered a wrong email, I cant find it anywhere.",
+                            type: "error",
+                            show: true,
+                        });
+                    }
+                },
+            );
+    } else {
+        sending.value = false;
+    }
 }
 </script>
 <template>
     <div class="contact-form">
-        <div class="form">
+        <div>
             <Alert :show="alertShow" :description="alertDescription" :type="alertType" :title="alertTitle" />
-            <div class="flex">
-                <input v-model="form.name" type="text" placeholder="Name" />
-                <input v-model="form.email" type="email" placeholder="Email" />
+            <div class="flex sm:flex-row flex-col text-left items-center gap-5 mb-5">
+                <div class="flex-1 w-full">
+                    <label>Name</label><br />
+                    <input
+                        v-model="form.name"
+                        type="text"
+                        placeholder="ex. Jenuel Dev"
+                        class="rounded-1 border-[var(--lightBackground)] bg-[var(--lightBackground)] text-amber-1 py-3 px-5 w-full"
+                    />
+                </div>
+                <div class="flex-1 w-full">
+                    <label>Email</label><br />
+                    <input
+                        v-model="form.email"
+                        type="email"
+                        placeholder="ex. me@jenuel.dev"
+                        class="rounded-1 border-[var(--lightBackground)] bg-[var(--lightBackground)] text-amber-1 py-3 px-5 w-full"
+                    />
+                </div>
             </div>
-            <input v-model="form.subject" type="text" placeholder="Subject" />
-            <textarea v-model="form.message" name="msg" placeholder="Message" rows="8"></textarea>
+            <div class="text-left mb-5">
+                <label>Subject</label>
+                <input
+                    v-model="form.subject"
+                    type="text"
+                    placeholder="What is this message about?"
+                    class="rounded-1 border-[var(--lightBackground)] bg-[var(--lightBackground)] text-amber-1 py-3 px-5 w-full"
+                />
+            </div>
+            <div class="text-left">
+                <label>Message</label><br />
+                <textarea
+                    v-model="form.message"
+                    name="msg"
+                    placeholder="Let us know what’s on your mind."
+                    rows="8"
+                    class="rounded-1 border-[var(--lightBackground)] bg-[var(--lightBackground)] text-amber-1 py-3 px-5 w-full"
+                ></textarea>
+            </div>
             <div class="mt-10px">
-                <button type="button"
+                <button
+                    type="button"
                     class="bg-[var(--primary)] px-25px py-10px rounded-md text-[var(--background)] font-600 opacity-90 hover:opacity-100 w-[100%]"
-                    :class="{ 'opacity-50 hover:opacity-50 cursor-not-allowed': sending }" @click="sendEmail()"
-                    :disabled="sending">
-                    <p style="margin: 0">{{ sending ? 'Sending...' : 'Send' }}</p>
+                    :class="{ 'opacity-50 hover:opacity-50 cursor-not-allowed': sending }"
+                    @click="sendEmail()"
+                    :disabled="sending"
+                >
+                    <p style="margin: 0">{{ sending ? "Sending..." : "Send" }}</p>
                 </button>
             </div>
         </div>
     </div>
 </template>
-
-<style lang="scss" src="./Style.scss"></style>
