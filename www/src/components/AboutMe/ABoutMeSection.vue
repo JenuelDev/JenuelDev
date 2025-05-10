dd94e0a9e9f5a9b85124b8f0fb4865482fc16bd1
Jenuel Oras Ganawed <me@jenuel.dev>
2025-05-10 19:26:53 +0800

    Updates

--- www/src/components/AboutMe/ABoutMeSection.vue
+++ www/src/components/AboutMe/ABoutMeSection.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
-import SvgDecoration from './../SvgDecoration/SvgDecoration.vue';
-import { Icon } from '@iconify/vue';
+import SvgDecoration from "./../SvgDecoration/SvgDecoration.vue";
+import YoutubeButton from "@/components/Button/YoutubeButton.vue";
+import { Icon } from "@iconify/vue";
 
 const yearCount = () => {
     return new Date().getFullYear() - 2018;
@@ -12,59 +13,72 @@ const yearCount = () => {
             <div class="lg:order-1 order-2">
                 <div class="lg:text-right text-center max-w-500px">
                     <div>
-                        <h3 class="md:text-3xl text-2xl font-500 mb-2"> Hi! I’m Jenuel, and I'm a</h3>
-                        <h1 class="md:text-5xl text-3xl font-600 text-[var(--primary)] mb-2">
-                            Software Developer
-                        </h1>
+                        <h3 class="md:text-3xl text-2xl font-500 mb-2">Hi! I’m Jenuel, and I'm a</h3>
+                        <h1 class="md:text-5xl text-3xl font-600 text-[var(--primary)] mb-2">Software Developer</h1>
                         <h2 class="md:text-3xl text-2xl text-[var(--lightestSlate)] mb-2">
                             I create web apps that are engaging, accessible and scalable.
                         </h2>
                     </div>
                     <div class="mt-30px flex lg:justify-end justify-center flex-wrap gap-20px items-center">
                         <div>
-                            <a href="https://www.linkedin.com/in/jenuelganawed/" target="_blank"
-                                class="bg-[var(--primary)] px-25px py-10px rounded-md text-[var(--background)] font-600 opacity-90 hover:opacity-100 flex items-center gap-5px">
+                            <a
+                                href="https://www.linkedin.com/in/jenuelganawed/"
+                                target="_blank"
+                                class="bg-[var(--primary)] px-25px py-10px rounded-md text-[var(--background)] font-600 opacity-90 hover:opacity-100 flex items-center gap-5px"
+                            >
                                 <Icon icon="tabler:brand-linkedin-filled" class="text-2xl" />
                                 <span> Get In Touch </span>
                             </a>
                         </div>
                         <div>
-                            <a href="/Jenuel Ganawed - Resume.pdf" target="_blank"
-                                class="bg-[var(--primary)] px-25px py-10px rounded-md text-[var(--background)] font-600 opacity-90 hover:opacity-100 cursor-pointer whitespace-nowrap flex items-center gap-5px">
+                            <a
+                                href="/Jenuel Ganawed - Resume.pdf"
+                                target="_blank"
+                                class="bg-[var(--primary)] px-25px py-10px rounded-md text-[var(--background)] font-600 opacity-90 hover:opacity-100 cursor-pointer whitespace-nowrap flex items-center gap-5px"
+                            >
                                 <Icon icon="tabler:file-type-doc" class="text-2xl" />
                                 <span> Resume </span>
                             </a>
                         </div>
+                        <YoutubeButton />
                     </div>
                 </div>
             </div>
             <div class="lg:order-2 order-1 z-50 relative">
                 <div class="overflow-hidden rounded-lg">
                     <div style="position: relative; overflow: hidden">
-                        <img class="max-w-300px w-[100%] w-[300px] profile-picture-style" loading="lazy" width="300px"
-                            src="./../../assets/images/profileImage.webp" alt="Jenuel Ganawed - Profile Picture" />
+                        <img
+                            class="max-w-300px w-[100%] w-[300px] profile-picture-style"
+                            loading="lazy"
+                            width="300px"
+                            src="./../../assets/images/profileImage.webp"
+                            alt="Jenuel Ganawed - Profile Picture"
+                        />
                     </div>
                 </div>
             </div>
             <SvgDecoration
                 classNames="absolute md:visible invisible left-[-50px] bottom-[-40px] transform rotate-[-180deg] fill-[var(--primary)] opacity-50"
-                :type="1" />
+                :type="1"
+            />
             <SvgDecoration
                 classNames="absolute md:right-[-56px] md:visible invisible z-20 fill-[var(--primary)] opacity-50"
-                :type="2" />
+                :type="2"
+            />
         </div>
-        <div v-scrollanimation
+        <div
+            v-scrollanimation
             class="md:text-2xl text-xl md:px-10px w-[100%] max-w-[770px] pt-100px md:leading-relaxed leading-relaxed relative"
-            style="transition-delay: 100ms;">
+            style="transition-delay: 100ms"
+        >
             <p class="mb-30px">
                 Back in 2016, I decided to make a big change, switching from studying criminology to diving into the
                 exciting world of Information Technology. That's when I discovered my passion for software development.
             </p>
             <p class="mb-30px">
                 Now, with over <strong>{{ yearCount() }} years of experience as a software developer</strong>, I've had
-                the chance to
-                work in a constantly changing field, collaborating with other coding enthusiasts and contributing to
-                some amazing projects.
+                the chance to work in a constantly changing field, collaborating with other coding enthusiasts and
+                contributing to some amazing projects.
             </p>
             <p class="mb-30px">
                 <strong>I'm all about the details</strong>, whether it's writing efficient code or perfecting a recipe.
@@ -78,7 +92,8 @@ const yearCount = () => {
             </p>
             <SvgDecoration
                 classNames="absolute lg:-right-30 -right-20 top-90 md:block hidden z-20 fill-[var(--primary)] opacity-50"
-                :type="3" />
+                :type="3"
+            />
         </div>
     </section>
 </template>
---
+++ www/src/components/Button/YoutubeButton.vue
@@ -0,0 +1,43 @@
+<script setup lang="ts">
+import { Icon } from "@iconify/vue";
+</script>
+<template>
+    <div class="relative group px-21px py-21px">
+        <a
+            href="https://www.youtube.com/@JenuelDev"
+            target="_blank"
+            class="bg-[var(--primary)] px-10px py-10px rounded-md text-[var(--background)] font-600 opacity-90 hover:opacity-100 cursor-pointer whitespace-nowrap flex items-center gap-5px shadow-sm z-100 absolute top-0 left-0 !group-hover:bg-red-5 transition-all"
+        >
+            <Icon icon="tabler:brand-youtube-filled" class="text-2xl !group-hover:text-dark-6 transition-all" />
+        </a>
+        <!-- group-hover:visible invisible -->
+        <div
+            class="group-hover:visible invisible absolute z-5 group-hover:-top-40px top-0 group-hover:-left-100px left-0 rotate-30 bg-[var(--primary)] text-[var(--background)] px-4 py-1 rounded-md border border-dark-9 transition-all delay-0 opacity-0 group-hover:opacity-100 scale-0 group-hover:scale-100"
+        >
+            Finance
+        </div>
+        <div
+            class="group-hover:visible invisible absolute z-3 group-hover:-top-5px top-0 group-hover:-left-90px left-0 rotate-10 bg-[var(--primary)] text-[var(--background)] px-4 py-1 rounded-md shadow-sm border border-dark-9 transition-all delay-50 opacity-0 group-hover:opacity-100 scale-0 group-hover:scale-100"
+        >
+            Tech
+        </div>
+        <div
+            class="group-hover:visible invisible absolute z-2 group-hover:top-30px top-0 group-hover:-left-150px -left-15px -rotate-10 bg-[var(--primary)] text-[var(--background)] px-4 py-1 rounded-md border border-dark-9 transition-all delay-150 opacity-0 group-hover:opacity-100 scale-0 group-hover:scale-100"
+        >
+            Programming
+        </div>
+        <div
+            class="group-hover:visible invisible absolute z-99 group-hover:top-40px top-0 group-hover:-right-110px right-0 rotate-10 bg-[var(--primary)] text-[var(--background)] px-4 py-1 rounded-md shadow-sm border border-dark-9 transition-all delay-100 opacity-0 group-hover:opacity-100 scale-0 group-hover:scale-100"
+        >
+            Business
+        </div>
+        <div
+            class="group-hover:visible invisible absolute z-99 group-hover:top-0px top-0 group-hover:-right-90px right-0 -rotate-10 bg-[var(--primary)] text-[var(--background)] px-4 py-1 rounded-md shadow-sm border border-dark-9 transition-all delay-125 opacity-0 group-hover:opacity-100 scale-0 group-hover:scale-100"
+        >
+            Code
+        </div>
+        <div
+            class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 group-hover:w-72 w-0 group-hover:h-72 h-0 rounded-full bg-[radial-gradient(circle,_rgba(0,0,0,0.5)_0%,_rgba(0,0,0,0.3)_40%,_rgba(0,0,0,0)_100%)] transition-all"
+        ></div>
+    </div>
+</template>
