676e855ff07bccf5cd1113853cb329daf9ae88a6
Jenuel Oras Ganawed <me@jenuel.dev>
2025-12-22 15:47:16 +0800

    Update TechnicalSkills.vue

--- www/src/components/AboutMe/TechnicalSkills.vue
+++ www/src/components/AboutMe/TechnicalSkills.vue
@@ -1,13 +1,13 @@
 <script setup lang="ts">
-import { Icon } from '@iconify/vue';
-import { skillList, mySkills } from '../../constant/TechnicalSkills';
-import { reactive, ref } from 'vue';
+import { Icon } from "@iconify/vue";
+import { skillList, mySkills } from "../../constant/TechnicalSkills";
+import { reactive, ref } from "vue";
 const dialog = ref(false);
 const dialogContent = reactive({
-    title: '',
-    des: '',
-    icon: '',
-    skill: '',
+    title: "",
+    des: "",
+    icon: "",
+    skill: "",
 });
 
 const openDialog = (skill: any) => {
@@ -16,18 +16,16 @@ const openDialog = (skill: any) => {
     dialogContent.des = skill.des;
     dialogContent.icon = skill.icon;
 
-    const mainScreenWindow = document.getElementById('main-screen-window');
+    const mainScreenWindow = document.getElementById("main-screen-window");
 
-    if (mainScreenWindow)
-        mainScreenWindow.style.overflowY = 'hidden';
+    if (mainScreenWindow) mainScreenWindow.style.overflowY = "hidden";
 
     dialog.value = !dialog.value;
 };
 const closeDialog = () => {
-    const mainScreenWindow = document.getElementById('main-screen-window');
+    const mainScreenWindow = document.getElementById("main-screen-window");
 
-    if (mainScreenWindow)
-        mainScreenWindow.style.overflowY = 'overlay';
+    if (mainScreenWindow) mainScreenWindow.style.overflowY = "overlay";
 
     dialog.value = !dialog.value;
 };
@@ -39,8 +37,11 @@ const closeDialog = () => {
             <transition name="modal">
                 <div class="box" v-show="dialog">
                     <div>
-                        <Icon @click="closeDialog()" class="x-close-button text-3xl"
-                            icon="material-symbols:close-rounded" />
+                        <Icon
+                            @click="closeDialog()"
+                            class="x-close-button text-3xl"
+                            icon="material-symbols:close-rounded"
+                        />
                         <h1 class="flex items-center">
                             <Icon class="skill-icon text-3xl" :icon="dialogContent.icon"></Icon>
                             <div>
@@ -59,8 +60,13 @@ const closeDialog = () => {
         </transition>
     </div>
     <ul v-show="false">
-        <li v-for="(skill, i) in skillList as any" v-scrollanimation :key="skill.text" @click="openDialog(skill)"
-            :style="'transition-delay: ' + i * 50 + 'ms'">
+        <li
+            v-for="(skill, i) in skillList as any"
+            v-scrollanimation
+            :key="skill.text"
+            @click="openDialog(skill)"
+            :style="'transition-delay: ' + Number(i) * 50 + 'ms'"
+        >
             <div>
                 <Icon class="skill-icon" :icon="skill.icon" :size="20" />
                 {{ skill.text }}
@@ -68,32 +74,45 @@ const closeDialog = () => {
         </li>
     </ul>
     <section class="flex flex-col w-[100%] max-w-[1000px] visible opacity-100 z-10 px-10px mx-auto">
-        <h1 v-scrollanimation
-            class="lg:text-size-52px md:text-size-44px text-size-36px font-600 text-[var(--primary)] tracking-tight mt-10px mb-15px">
+        <h1
+            v-scrollanimation
+            class="lg:text-size-52px md:text-size-44px text-size-36px font-600 text-[var(--primary)] tracking-tight mt-10px mb-15px"
+        >
             Technical skill set
         </h1>
-        <p v-scrollanimation
-            class="md:text-size-24px text-size-20px w-[100%] max-w-[770px] md:leading-relaxed leading-relaxed">
+        <p
+            v-scrollanimation
+            class="md:text-size-24px text-size-20px w-[100%] max-w-[770px] md:leading-relaxed leading-relaxed"
+        >
             I am continuously learning to keep up with the latest technologies and be able to pick the best tech for the
             job.
         </p>
-        <div class="technical-skills-wrapper mt-30px flex flex-wrap  sm:gap-10 gap-0 sm:px-5">
-            <div v-scrollanimation class="flex flex-col justify-between" v-for="mySkill in mySkills"
-                :key="mySkill.tech">
+        <div class="technical-skills-wrapper mt-30px flex flex-wrap sm:gap-10 gap-0 sm:px-5">
+            <div
+                v-scrollanimation
+                class="flex flex-col justify-between"
+                v-for="mySkill in mySkills"
+                :key="mySkill.tech"
+            >
                 <div>
                     <div class="flex items-center">
                         <Icon :icon="mySkill.icon" class="text-2xl" />
                         <div class="ml-7px font-600 md:text-size-24px text-size-20px">{{ mySkill.tech }}</div>
                         <span class="technical-skill-year md:text-size-20px text-size-18px opacity-60">{{
-                            mySkill.yearStarted }}</span>
+                            mySkill.yearStarted
+                        }}</span>
                     </div>
                     <div class="md:text-size-20px text-size-18px md:leading-normal leading-normal py-[1rem]">
                         {{ mySkill.des }}
                     </div>
                 </div>
                 <div class="text-size-14px flex items-center gap-5px flex-wrap">
-                    <div class=" p-5px rounded-md cursor-pointer flex items-center gap-5px text-size-20px duration-100 truncate scale-100 hover:scale-103"
-                        v-for="tool in mySkill.tools" :key="tool.text" @click="openDialog(tool)">
+                    <div
+                        class="p-5px rounded-md cursor-pointer flex items-center gap-5px text-size-20px duration-100 truncate scale-100 hover:scale-103"
+                        v-for="tool in mySkill.tools"
+                        :key="tool.text"
+                        @click="openDialog(tool)"
+                    >
                         <Icon class="skill-icon min-w-30px text-3xl" :icon="tool.icon"></Icon>
                         <span class="ml-10px">{{ tool.text }}</span>
                     </div>
@@ -106,7 +125,7 @@ const closeDialog = () => {
 <style lang="scss">
 .technical-skill-year {
     &::before {
-        content: '\2022';
+        content: "\2022";
         color: #a0aec0;
         margin-left: 0.5rem;
         margin-right: 0.5rem;
@@ -115,7 +134,7 @@ const closeDialog = () => {
 }
 
 .technical-skills-wrapper {
-    &>* {
+    & > * {
         padding-top: 15px;
         padding-bottom: 15px;
         flex: 1 1 300px;
